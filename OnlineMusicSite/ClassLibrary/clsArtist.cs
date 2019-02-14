e4a6888a42345d98dea2233c38d74f363e6e48b8
p17164113 <p17164113@GWHXX653WIN016W>
2019-02-14 09:21:45 +0000

    ArtistTestProject

---
+++ OnlineMusicSite/ClassLibrary/clsArtist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsArtist
+    {
+        public clsArtist()
+        {
+        }
+
+        public string Artist { get; set; }
+        public int ArtistNo { get; set; }
+
+        public string Valid(string someArtist)
+        {
+            string Error = "";
+            if (someArtist.Length > 50)
+            {
+                Error = "The Artist Name cannot be more then 50 characters";
+            }
+            if (someArtist.Length == 0)
+            {
+                Error = "The Artist Name may not be blank";
+            }
+            return Error;
+        }
+    }
+}
