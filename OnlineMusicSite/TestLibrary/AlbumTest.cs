68b790dc6681cc2cd681fd90b8c65e353c8d5ce8
Emran Ali <P17152562@my365.dmu.ac.uk>
2019-02-16 18:28:42 +0000

    clsAlbum

---
+++ OnlineMusicSite/TestLibrary/AlbumTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class AlbumTest
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the clsalbum 
+            clsAlbum AnAblum = new clsAlbum();
+            //test to see that it exists 
+            Assert.IsNotNull(AnAblum);
+        }
+    }
+}
