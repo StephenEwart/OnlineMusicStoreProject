4ad6d69aa6a4b37a76e4e7dc779c087579de6b93
RhysCruz <rhyscruz16@gmail.com>
2019-02-17 18:00:24 +0000

    Test Collection class

--- OnlineMusicSite/ClassLibrary/clsArtistCollection.cs
+++ OnlineMusicSite/ClassLibrary/clsArtistCollection.cs
@@ -8,7 +8,38 @@ namespace ClassLibrary
 {
     public class clsArtistCollection
     {
-        public int Count { get; set; }
-        public List<clsArtist> AllArtists { get; set; }
+        //private data member for the allartists list
+        private List<clsArtist> mAllArtists = new List<clsArtist>();
+
+        public int Count
+        {
+            get
+            {
+                //return the count property of the private list
+                return mAllArtists.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public List<clsArtist> AllArtists
+        {
+            get
+            {
+                //return the count property of the private list
+                return mAllArtists;
+            }
+            set
+            {
+                //assign the incoming value to the private data member
+                mAllArtists = value;
+            }
+        }
     }
 }
+    
+
+
+
+
