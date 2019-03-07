87685103b6640c0e038d640a0fc1e498acd0f7a1
p17164113 <p17164113@GWHXX653WIN016W>
2019-03-07 10:48:19 +0000

    Page 74

--- OnlineMusicSite/ClassLibrary/clsArtist.cs
+++ OnlineMusicSite/ClassLibrary/clsArtist.cs
@@ -8,12 +8,31 @@ namespace ClassLibrary
 {
     public class clsArtist
     {
+        //private data member for the ArtistNo property
+        private Int32 mArtistNo;
         public clsArtist()
         {
+            
         }
 
         public string Artist { get; set; }
-        public int ArtistNo { get; set; }
+        public int ArtistNo
+        {
+            get
+            {
+                //return the private data
+                return mArtistNo;
+            }
+
+            set
+            {
+                //set the value of the private data member
+                mArtistNo = value;
+            }
+        }
+        public string ArtistName { get; set; }
+        public string ArtistGenre { get; set; }
+        public string ArtistBio { get; set; }
 
         public string Valid(string someArtist)
         {
@@ -27,6 +46,14 @@ namespace ClassLibrary
                 Error = "The Artist Name may not be blank";
             }
             return Error;
+        }
+
+        public bool Find (int ArtistNo)
+        {
+            //set the private data member to the test data value
+            mArtistNo = 21;
+            //always return true
+            return true;
         }
     }
 }
