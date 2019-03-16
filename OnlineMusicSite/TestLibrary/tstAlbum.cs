1dd25bce9145a23236478f374cec620f6042ba41
Emran Ali <P17152562@my365.dmu.ac.uk>
2019-03-16 12:42:07 +0000

    p58

---
+++ OnlineMusicSite/TestLibrary/tstAlbum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class tstAlbum
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instnace of the class we want to create 
+            clsAlbum AnAlbum = new clsAlbum();
+            //test to see that it exists
+            Assert.IsNotNull(AnAlbum);
+        }
+    }
+}
