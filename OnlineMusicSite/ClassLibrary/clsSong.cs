7eb13b20cb48e8f2c105b67bac5e9b3cdf4e4c27
p16172570 <p16172570@mydmu.ac.uk>
2019-03-18 20:57:52 +0000

    Completed part of tests

--- OnlineMusicSite/ClassLibrary/clsSong.cs
+++ OnlineMusicSite/ClassLibrary/clsSong.cs
@@ -9,5 +9,26 @@ namespace ClassLibrary
     public class clsSong
     {
         public string Song { get; set; }
+        public int SongNo { get; set; }
+
+        public bool Valid(string SomeSong)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public string Valid(string someSong)
+    {
+        //if the name of the county is not blank
+        if (someSong !== "")
+        {
+            //return a blank string 
+            return "";
+        }
+        else
+        {
+            //otherwise return an error message 
+            return "The song name may not be blank!";
+        }
     }
 }
--- OnlineMusicSite/TestLibrary/SongTest.cs
+++ OnlineMusicSite/TestLibrary/SongTest.cs
@@ -20,13 +20,55 @@ namespace TestLibrary
         public void SongPropertyOK()
         {
             //create an instance of the class we want to create
-            clsSong Asong = new clsSong();
+            clsSong ASong = new clsSong();
             //create some test data to asssign to the property
             string SomeSong = "Rockstar";
             //assign the data to the property
-            Asong.Song = SomeSong;
+            ASong.Song = SomeSong;
             //test to see that the two values are the same
-            Assert.AreEqual(Asong.Song, SomeSong);
+            Assert.AreEqual(ASong.Song, SomeSong);
         }
+
+        [TestMethod]
+        public void SongNoPropertyOKK()
+        {
+            //create an instance of the class we want to create
+            clsSong ASong = new clsSong();
+            //create some test data t assign to the property 
+            Int32 SongNo = 1;
+            //assign the data to the property 
+            ASong.SongNo = SongNo;
+            //test to see that the two values are the same
+            Assert.AreEqual(ASong.SongNo, SongNo);
+        }
+
+        clsSong ASong = new clsSong();
+        string NewSong = txtSong.Text;
+        if (clsSong.Valid(NewSong) == "")
+        {
+            //do something with the data 
+        }
+        else
+        {
+            //display an error message
+        }
+
+        [TestMethod]
+        public void ValidMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsSong ASong = new clsSong();
+            //create an string variable to store the result of the validation 
+            String Error = "";
+            //create some test data to test the method 
+            string SomeSong = "Difference";
+            //invoke the method
+            Error = ASong.Valid(SomeSong);
+            //test to see that the result is OK i.e. there was no error message returned 
+            Assert.AreEqual(Error, "");
+        }
+
+
     }
+
 }
